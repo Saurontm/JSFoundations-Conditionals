33a44975f8e175b03b1b98430b64dc3222f580a8
Sarah Almahmid <hpsarontm@gmail.com>
2021-05-24 18:50:11 +0300

    done with the task, after modifications!

--- calculator.js
+++ calculator.js
@@ -1,13 +1,36 @@
 const prompt = require("prompt-sync")(); // Please don't touch me :)
 
-//two integers (used parseInt to convert from string to integer, i googled it)
-const integer1 = parseInt(prompt("Please enter the first number: "));
-const integer2 = parseInt(prompt("Please enter the second number: "));
+//initial task:
+// //two integers (used parseInt to convert from string to integer, i googled it)
+// const integer1 = parseInt(prompt("Please enter the first number: "));
+// const integer2 = parseInt(prompt("Please enter the second number: "));
+
+// //operation
+// const op = prompt("Please choose the operation (+, -, /, *): ");
+
+// //check operation and perform 
+// if (op === "+"){
+//     console.log(`${integer1} + ${integer2} = ${integer1 + integer2}`);
+// } else if (op === "-"){
+//     console.log(`${integer1} - ${integer2} = ${integer1 - integer2}`);
+// } else if (op === "/"){
+//     console.log(`${integer1} / ${integer2} = ${integer1 / integer2}`);
+// } else if (op === "*"){
+//     console.log(`${integer1} * ${integer2} = ${integer1 * integer2}`);
+// } else {
+//     console.log(`can't you read??`);
+// }
+
+
+//after fix: Everything is hard-coded
+
+//two integers 
+const integer1 = 4;
+const integer2 = 5;
 
 //operation
-const op = prompt("Please choose the operation (+, -, /, *): ");
+const op = "*";
 
-//check operation and perform 
 if (op === "+"){
     console.log(`${integer1} + ${integer2} = ${integer1 + integer2}`);
 } else if (op === "-"){
